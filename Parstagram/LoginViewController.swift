3148d9ea2887abccff491fcf66b30b833d700edd
Hugo Meza <hgmeza114@gmail.com>
2019-11-15 18:45:11 -0500

    Implement Sign In

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -15,6 +15,17 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     
     @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) {
+            (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
         
     }
     
